c762125487e7c6637e196d889a8d795368f15f05
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2022-11-15 19:52:26 +0530

    commit new methods

---
+++ src/nov15/Sample.java
@@ -0,0 +1,25 @@
+package nov15;
+
+public class Sample {
+public void add()
+{
+	int a=987;
+int	b=987;
+int c=a+b;
+System.out.println(c);
+	
+}
+public void division()
+{
+	int a=987;
+int	b=987;
+int c=a/b;
+System.out.println(c);
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
