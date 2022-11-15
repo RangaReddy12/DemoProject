d22942e160c73ab246f3db3182fcea83906ace06
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2022-11-15 20:05:30 +0530

    commit new method

--- src/nov15/Sample.java
+++ src/nov15/Sample.java
@@ -17,6 +17,14 @@ int c=a/b;
 System.out.println(c);
 	
 }
+public void sub()
+{
+	int a=987;
+int	b=987;
+int c=a-b;
+System.out.println(c);
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
